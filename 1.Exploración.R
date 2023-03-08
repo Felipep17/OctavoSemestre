38385463a3e4433d277f229dc1b376cb4cd0dbf4
Andrés Felipe Palomino Montezuma <palomino.andres@correounivalle.edu.co>
2023-03-07 20:17:18 -0500

    Update 1.Exploración.R

--- 1.Exploración.R
+++ 1.Exploración.R
@@ -1,7 +1,7 @@
 ### Estacionariedad
 library(tseries)
 library(forecast)
-n<- 1:100
+n<- 1:1000
 mu<- 9-5*n
 abs(rnorm(100))
 X<- c(rbinom(100,10,0.5),rnorm(100,mu,abs(2*mu)),rgamma(100,abs(rnorm(100))+2,2),rpois(100,10),rweibull(100,abs(rnorm(100))+2,5))
