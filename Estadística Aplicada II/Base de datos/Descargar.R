3c019c9821fbf20593ab1e3eea9f58ef7db3d06a
Andrés Felipe Palomino Montezuma <palomino.andres@correounivalle.edu.co>
2023-03-12 09:51:31 -0500

    Update Descargar.R

--- Estadística Aplicada II/Base de datos/Descargar.R
+++ Estadística Aplicada II/Base de datos/Descargar.R
@@ -1,4 +1,3 @@
-library(pls)
 X<-as.data.frame(yarn$NIR)
 y<- as.data.frame(yarn$density)
 z<- as.data.frame(yarn$train)
@@ -12,3 +11,18 @@ X<- yarn
 ncol(yarn)
 plot(yarn[,1:10])
 plot(density~NIR.50,data=X,pch=19,panel.first=grid())
+x<- X[,50]
+summary(x)
+resumen<- function(x){
+  X<- matrix(0,9,1)
+  resumen<- round(c(mean(x),median(x),min(x),max(x),var(x),sd(x),quantile(x,0.25),quantile(x,0.75),sd(x)/mean(x)),4)
+  for( i  in 1:7){
+    X[i,]<- resumen[i]
+  }
+  rownames(X)<-c('Media','Mediana','Min','Max','Var','Sd','1st Qu.','3rd Qu','Coef.Var')
+  colnames(X)<- ('Estadística Descriptivas')
+  return(X)
+}
+resumen(x)
+library(xtable)
+xtable(resumen(x))
