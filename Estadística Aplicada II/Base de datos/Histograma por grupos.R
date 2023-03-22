db4f383d54dd9dca2f3080d6d71033a659f35863
Andrés Felipe Palomino Montezuma <palomino.andres@correounivalle.edu.co>
2023-03-21 20:01:02 -0500

    Create Histograma por grupos.R

---
+++ Estadística Aplicada II/Base de datos/Histograma por grupos.R
@@ -0,0 +1,13 @@
+x<- rnorm(1000,10,1)
+g<- as.factor(c(rep('1',50),rep('2',50)))
+Df<- as.data.frame(cbind(x,g))
+head(Df)
+mi.azul <- rgb(0, 0, 1, 0.3) # rgb(0, 0, 1, alpha = 0.3)
+mi.rojo <- rgb(1, 0, 0, 0.5)
+par(mfrow=c(1,1))
+hist(Df$x[Df$g=='1'],xlim=c(range(Df$x)),col=mi.azul)
+par(new=T)
+hist(Df$x[Df$g=='2'],xlim=c(range(Df$x)),axes=F,col=mi.rojo,xlab='',ylab='',main='')
+legend(x = "topright",legend=c("G1","G2"),pch=15
+       ,pt.cex=2,col=c(mi.azul,mi.rojo),
+       box.lwd=0.6,text.font =15,cex=1)
