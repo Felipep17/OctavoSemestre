a01b5b2088861d997078b5d1025c4043cea7e2eb
Andrés Felipe Palomino Montezuma <palomino.andres@correounivalle.edu.co>
2023-03-21 20:06:49 -0500

    Update Histograma por grupos.R

--- Estadística Aplicada II/Base de datos/Histograma por grupos.R
+++ Estadística Aplicada II/Base de datos/Histograma por grupos.R
@@ -5,9 +5,16 @@ head(Df)
 mi.azul <- rgb(0, 0, 1, 0.3) # rgb(0, 0, 1, alpha = 0.3)
 mi.rojo <- rgb(1, 0, 0, 0.5)
 par(mfrow=c(1,1))
+
 hist(Df$x[Df$g=='1'],xlim=c(range(Df$x)),col=mi.azul)
+# Grid vertical
+axis(1, tck = 1, lty = 2, col = "gray")
+
+# Grid horizontal
+axis(2, tck = 1, lty = 2, col = "gray")
 par(new=T)
 hist(Df$x[Df$g=='2'],xlim=c(range(Df$x)),axes=F,col=mi.rojo,xlab='',ylab='',main='')
 legend(x = "topright",legend=c("G1","G2"),pch=15
        ,pt.cex=2,col=c(mi.azul,mi.rojo),
        box.lwd=0.6,text.font =15,cex=1)
+
