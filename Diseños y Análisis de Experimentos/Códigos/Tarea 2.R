49548218fb9cfc70b8480326a73df23a848d7f07
Andrés Felipe Palomino Montezuma <palomino.andres@correounivalle.edu.co>
2023-04-20 14:00:59 -0500

    Actualizando

--- Diseños y Análisis de Experimentos/Códigos/Tarea 2.R
+++ Diseños y Análisis de Experimentos/Códigos/Tarea 2.R
@@ -4,14 +4,15 @@ length(Y)
 Y<-factor(Y)
 U<- as.data.frame(cbind(X,Y))
 colnames(U)<- c("Rendimiento Melon","Variedad")
-boxplot(U[,1]~U[,2],,xlab=paste(colnames(U)[2]),ylab=paste(colnames(U)[1]))
+boxplot(U[,1],panel.first=grid(),xlab=paste(colnames(U)[2]),ylab=paste(colnames(U)[1]))
 grid(nx = NULL, ny = NULL,
      lty = 1, col = "gray", lwd = 1)
-boxplot(U[,1]~U[,2],col=c('aquamarine1','yellow','orange','red'),pch=19,add=T)
+boxplot(U[,1],col=c('aquamarine1','yellow','orange','red'),pch=19,add=T)
+stripchart(U[,1], method = "jitter", pch = 19, add = TRUE, col = "blue")
 legend(x = "topright",legend=c("1","2","3","4"),
        col=c('aquamarine1','yellow','orange', 'red'),pt.cex=1,pch=15,title='Variedad',
        box.lwd=1,text.font =20,cex=0.8)
-U$Variedad
+?stripchart
 mean(U$`Rendimiento Melon`[U$Variedad=='1'])
 mean(U$`Rendimiento Melon`[U$Variedad=='2'])
 mean(U$`Rendimiento Melon`[U$Variedad=='3'])
@@ -36,4 +37,31 @@ Anova[2,]<-c("Error",21 , sum(U$`Rendimiento Melon`^2)-18544.15, (sum(U$`Rendimi
 Anova[3,]<-c(23,1290.301+sum(U$`Rendimiento Melon`^2)-18544.15,0,0,0)
 library(xtable)
 xtable(Anova)
-pf(23.36,3,21)
+options(scipen=999)
+1-pf(23.36,3,20)
+# Vertical box plot
+boxplot(U[,1]~U[,2],ylab='Rendimiento del Melón',xlab='Variedad' ,col = c('aquamarine1','yellow','orange','red'))
+grid(nx = NULL, ny = NULL,
+     lty = 1, col = "gray", lwd = 1)
+boxplot(U[,1]~U[,2], col = c('aquamarine1','yellow','orange','red'),add=T)
+# Points
+stripchart(U[,1]~U[,2],              # Data
+           method = "jitter", # Random noise
+           pch = 19,          # Pch symbols
+           col = 'black',           # Color of the symbol
+           vertical = TRUE,   # Vertical mode
+           add = TRUE)   
+######### Anova con R
+V1<-c(rep(1,6),rep(0,18))
+V2<-c(rep(0,6),rep(1,6),rep(0,12))
+V3<-c(rep(0,6),rep(0,6),rep(1,6),rep(0,6))
+V4<- c(rep(0,18),rep(1,6))
+X<- cbind(V1,V2,V3,V4)
+Y<- as.numeric(c(25.1,17.2,26.4,16.1,22.2,15.9,40.2,35.2,32,36.5,43.3,37.1,18.3,22.6,25.9,15,11.4,23.7,28.6,28,33.2,31.7,30.3,27.6))
+model<- aov(Y~X)
+library(ggfortify)
+autoplot(lm(Y~X))
+model$coefficients
+summary(model)
+Z<- cbind(U[1:6,1],U[7:12,1],U[13:18,1],U[19:24,1])
+colnames(Z)<- c('V1','V2','V3','V4')
