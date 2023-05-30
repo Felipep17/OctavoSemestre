21e31fa89b9d9a033f24fa31938e4883ec115676
Andrés Felipe Palomino Montezuma <palomino.andres@correounivalle.edu.co>
2023-05-30 13:19:21 -0500

    Actualizando

--- Modelos II/Códigos/Tarea 4.R
+++ Modelos II/Códigos/Tarea 4.R
@@ -1,3 +1,4 @@
+library(xtable)
 X<- AbusoDrogas
 names(X)
 X<- X[,-1]
@@ -5,25 +6,79 @@ pairs(X)
 str(X)
 options(scipen=999)
 modrugs.logit = glm(DFREE~AGE+BECK+NDRUGTX+TREAT,family=binomial(logit),data=X)
+p<- c()
+for(i in 1:length(coef(modrugs.logit))){
+p[i]<- exp(coef(modrugs.logit)[i])
+}
+cbind(coef(modrugs.logit),p)[-1,]
+xtable(cbind(coef(modrugs.logit),p)[-1,])
+xtable(summary(modrugs.logit))
+modrugs.logitinter = glm(DFREE~AGE*TREAT+BECK*TREAT+NDRUGTX*TREAT,family=binomial(logit),data=X)
+xtable(anova(modrugs.logit,modrugs.logitinter,test='LRT'))
+VarMat = vcov(modrugs.logitinter)
+Coef = coefficients(modrugs.logitinter)
+xtable(summary(modrugs.logitinter))
+wald.test(VarMat,Coef,Terms=c(6,7,8))
+library(aod)
+??anova
+summary(modrugs.logitinter)
+vif(modrugs.logit)
 modrugs.probit = glm(DFREE~AGE+BECK+NDRUGTX+TREAT,family=binomial(probit),data=X)
 modrugs.cloglog = glm(DFREE~AGE+BECK+NDRUGTX+TREAT,family=binomial(cloglog),data=X)
-summary(modrugs.logit)
+Mods<-round(cbind(coef(modrugs.logit),sqrt(diag(vcov(modrugs.logit))),coef(modrugs.logit)/sqrt(diag(vcov(modrugs.logit))),(1-pnorm(abs(coef(modrugs.logit)/sqrt(diag(vcov(modrugs.logit))))))*2,coef(modrugs.probit),sqrt(diag(vcov(modrugs.probit))),coef(modrugs.probit)/sqrt(diag(vcov(modrugs.probit))),(1-pnorm(abs(coef(modrugs.probit)/sqrt(diag(vcov(modrugs.probit))))))*2,coef(modrugs.cloglog),sqrt(diag(vcov(modrugs.cloglog))),coef(modrugs.cloglog)/sqrt(diag(vcov(modrugs.cloglog))),(1-pnorm(abs(coef(modrugs.cloglog)/sqrt(diag(vcov(modrugs.cloglog))))))*2),4)
+rbind(0,0,Mods)
+name<-c("Est","Std","Z", "P-Val")
+Mods[1,]<-c(name,name,name)
+
+crit<- cbind(rbind(AIC(modrugs.logit),deviance(modrugs.logit)),rbind(AIC(modrugs.probit),deviance(modrugs.probit)),rbind(AIC(modrugs.cloglog),deviance(modrugs.cloglog)))
+colnames(crit)<- c("Logit","Probit","CLogLog")
+rownames(crit)<- c("AIC","Devianza")
+xtable(crit)
+xtable(Mods)
+summary(modrugs.cloglog)
+y<-c()
+x<-coef(modrugs.probit)/sqrt(diag(vcov(modrugs.probit)))
+for( i in 1:length(x)){
+  y[i]<- (1-pnorm(abs(x[i])))*2
+  
+}
+
+(1-pnorm(1.24))*2
 summary(modrugs.probit)
 summary(modrugs.cloglog)
 deviance(modrugs.logit)
 deviance(modrugs.probit)
 deviance(modrugs.cloglog)
 library(pROC)
+?roc
 ROCbw.logit = roc(X$DFREE~modrugs.logit$fitted.values)
 ROCbw.probit = roc(X$DFREE~modrugs.probit$fitted.values)
 ROCbw.loglog = roc(X$DFREE~modrugs.cloglog$fitted.values)
-plot(ROCbw.logit)
-lines(ROCbw.probit,col=2)
-lines(ROCbw.loglog,col=3)
+x11()
+?roc
+plot(ROCbw.logit, print.thres = "best", print.auc = TRUE,
+     auc.polygon = FALSE, max.auc.polygon = FALSE, auc.polygon.col = "gainsboro",
+     col = 2, grid = TRUE,xlim=c(1,0))
+metricas<- cbind(rbind(0.633,0.453,0.776,0.235),rbind(0.631,0.435,0.782,0.233),rbind(0.633,0.465,0.776,0.235))
+colnames(metricas)<- c("Logit","Probit","CLogLog")
+rownames(metricas)<- c("AUC","Sensibilidad","Especificidad","Punto de corte")
+xtable(metricas)
+ROCbw.logit
+
+plot(ROCbw.probit,col=1, print.thres = "best", print.auc = TRUE,
+      auc.polygon = FALSE, max.auc.polygon = FALSE, auc.polygon.col = "gainsboro"
+      , grid = TRUE)
+plot(ROCbw.loglog,col=3, print.thres = "best", print.auc = TRUE,
+      auc.polygon = FALSE, max.auc.polygon = FALSE, auc.polygon.col = "gainsboro"
+      , grid = TRUE)
+legend(x = "topleft",legend=c("Logit","Probit","CLogLog"),
+       col=c(1,2,3),lwd=2,title='Enlace',
+       box.lwd=1,text.font =20,cex=0.8)
 #####
 library(pROC)
 # Cálculo de la curva ROC y determinación del mejor punto de corte usando el método de Youden
 ROCbw.logit <- roc(X$DFREE, modrugs.logit$fitted.values)
+coords(ROCbw.logit, "best",best.method="closest.topleft")
 cutoff_youden_logit <- coords(ROCbw.logit, "best", best.method = "youden")
 sensitivity_youden_logit <- cutoff_youden_logit$sensitivity
 specificity_youden_logit <- cutoff_youden_logit$specificity
